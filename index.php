d88a0702383c9037e6f129b6cab68f3241bc9057
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.fr>
2023-03-28 12:48:24 +0200

    correction

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php
 
+require_once 'src/Fighter.php';
+
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+$heracles = new Fighter('🧔 Héraclès', 20, 6);
+$lion = new Fighter('🦁 Lion de Némée', 11, 13);
+
+$round = 1;
+while ($heracles->isAlive() && $lion->isAlive()) {
+    echo '🕛 Round : ' . $round . PHP_EOL;
+    $heracles->fight($lion);
+    echo $heracles->getName() . '🗡️' . $lion->getName() . '. 💙 ' . $lion->getLife() . PHP_EOL;
+    if ($lion->isAlive()) {
+        $lion->fight($heracles);
+        echo $lion->getName() . '🗡️' . $heracles->getName() . '. 💙 ' . $heracles->getLife() . PHP_EOL;
+    }
+    $round++;
+}
+
+if (!$heracles->isAlive()) {
+    $loser = $heracles->getName();
+    $winner = $lion->getName();
+} else {
+    $loser = $lion->getName();
+    $winner = $heracles->getName();
+}
+
+echo '🏆 ' . $winner . PHP_EOL;
+echo '💀 ' . $loser . PHP_EOL;
--- src/Fighter.php
+++ src/Fighter.php
@@ -1,3 +1,58 @@
 <?php
 
-/* Fighter class definition */
+class Fighter
+{
+    public const MAX_LIFE = 100;
+
+    private string $name;
+    private int $strength;
+    private int $dexterity;
+    private int $life = self::MAX_LIFE;
+
+    public function __construct(string $name, int $strength, int $dexterity)
+    {
+        $this->name = $name;
+        $this->strength = $strength;
+        $this->dexterity = $dexterity;
+    }
+
+    public function fight(Fighter $defender): void
+    {
+        $damage = rand(1, $this->getStrength()) - $defender->getDexterity();
+        if($damage < 0) {
+            $damage = 0;
+        }
+
+        $defender->setLife($defender->getLife() - $damage);
+    }
+
+    public function getStrength(): int
+    {
+        return $this->strength;
+    }
+  
+    public function getDexterity(): int
+    {
+        return $this->dexterity;
+    }
+  
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getLife(): int 
+    {
+        return $this->life;
+    }
+
+    public function setLife(int $life): void
+    {
+        $this->life = $life;
+    }
+
+    public function isAlive(): bool 
+    {
+        return $this->getLife() > 0;
+    }
+}
